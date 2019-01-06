1a4a747de09e970c7e56b61fdbd74a1fbc8fc993
Daniel Keller <jobe451@gmail.com>
2019-01-07 00:40:39 +0100

    first fully functional version

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import {LoremWhateverGenerator, PredefinedGenerators} from './lorem';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
-const defaultMinWordSize = 200;
+let defaultMinWordSize = 200;
 const generatorsMap = new Map();
 
 export function activate(context: vscode.ExtensionContext) {
@@ -30,6 +30,40 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand('loremWhatever.loremGenesis', () => {
 			runGenerator(PredefinedGenerators.GENESIS);
 		}),	
+		vscode.commands.registerCommand('loremWhatever.loremCustom1', () => {
+			runGenerator("Custom1");
+		}),	
+		vscode.commands.registerCommand('loremWhatever.loremCustom2', () => {
+			runGenerator("Custom2");
+		}),	
+		vscode.commands.registerCommand('loremWhatever.loremCustom3', () => {
+			runGenerator("Custom3");
+		}),	
+		vscode.commands.registerCommand('loremWhatever.loremCustom4', () => {
+			runGenerator("Custom4");
+		}),	
+		vscode.commands.registerCommand('loremWhatever.loremCustom5', () => {
+			runGenerator("Custom5");
+		}),	
+		vscode.commands.registerCommand('loremWhatever.loremDefault', () => {
+			let defaultLoremName = <string>vscode.workspace.getConfiguration('loremWhatever').get("defaultLorem");
+			let generatorType: PredefinedGenerators|string;
+
+			if      (defaultLoremName === "Genesis")       {generatorType = PredefinedGenerators.GENESIS}
+			else if (defaultLoremName === "Faust")         {generatorType = PredefinedGenerators.FAUST}
+			else if (defaultLoremName === "Gibson")        {generatorType = PredefinedGenerators.GIBSON}
+			else if (defaultLoremName === "Constitution")  {generatorType = PredefinedGenerators.CONSTITUTION}
+			else if (defaultLoremName === "Basel 3")       {generatorType = PredefinedGenerators.BASEL3}
+			else if (defaultLoremName === "Buzzword")      {generatorType = PredefinedGenerators.BUZZWORD}
+			else if (defaultLoremName === "Custom 1")      {generatorType = "Custom1"}
+			else if (defaultLoremName === "Custom 2")      {generatorType = "Custom2"}
+			else if (defaultLoremName === "Custom 3")      {generatorType = "Custom3"}
+			else if (defaultLoremName === "Custom 4")      {generatorType = "Custom4"}
+			else if (defaultLoremName === "Custom 5")      {generatorType = "Custom5"}
+			else                                           {generatorType = PredefinedGenerators.GIBSON}
+
+			runGenerator(generatorType);
+		}),	
 	];
 
 	commands.forEach((command) => {
@@ -37,16 +71,35 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 }
 
-function runGenerator(mapType: PredefinedGenerators) {
+function runGenerator(mapType: PredefinedGenerators|string): void {
 	let generator: LoremWhateverGenerator;
 	
 	generator = generatorsMap.get(mapType);
 	if(generator === undefined)  {
-		generator = new LoremWhateverGenerator(mapType);
-		generatorsMap.set(mapType, generator);
+		if(typeof mapType  === "string") {
+			let configCustomArray = [];
+			let customLorems = vscode.workspace.getConfiguration('loremWhatever').get("customLorems");
+			if (customLorems) {
+				configCustomArray = (<any>customLorems)[mapType];
+			}
+
+			if (configCustomArray instanceof Array && configCustomArray.length > 0) {
+				generator = new LoremWhateverGenerator(configCustomArray);
+			}
+			else {
+				vscode.window.showInformationMessage("lorem type " + mapType + " is either undefined or empty");
+				return;
+			}
+		}
+		else {
+			generator = new LoremWhateverGenerator(mapType);
+		}
+		generatorsMap.set(mapType, generator);	
 	}
 
-	let loremText = generator.getSentencesByMinWordCount(defaultMinWordSize);
+	let configMinWordSize = <number>vscode.workspace.getConfiguration('loremWhatever').get("minWordCount");
+	
+	let loremText = generator.getSentencesByMinWordCount(configMinWordSize ? configMinWordSize : defaultMinWordSize);
 	insertText(loremText);
 }
 
